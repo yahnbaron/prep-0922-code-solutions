34c4c7d7b00121b9018bf33247ba83c0f6569fad
Baron Yahn <yahnbaron@me.com>
2022-09-27 05:22:07 +0000

    Corrected the arithmetic (or lack thereof) in the assignment of the lastElement variable.

--- prep-javascript-arrays/main.js
+++ prep-javascript-arrays/main.js
@@ -6,5 +6,5 @@ var thirdElement = languages[2];
 console.log('the third element of the array is ' + thirdElement);
 var length = languages.length;
 console.log('the length of the array is ' + length);
-var lastElement = languages[5];
+var lastElement = languages[length - 1];
 console.log('the last element of the array is ' + lastElement);

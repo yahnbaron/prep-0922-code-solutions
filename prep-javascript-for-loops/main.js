0bdff12d4991b1a169f5b98d4edf5ff6cede8162
Baron Yahn <yahnbaron@me.com>
2022-09-29 05:18:41 +0000

    Completed prep-javascript-for-loops exercise.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,12 @@
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (var i = 0; i < 20; i++) {
+  console.log(i);
+  i = i + 1;
+}
+
+for (var i = 100; i > 0; i--) {
+  console.log('Time till explosion: ' + i + '!');
+}
